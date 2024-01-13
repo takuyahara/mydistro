1cc372889adcb80ae5706de09263503c56e17b50
takuyahara <h.taku86@gmail.com>
2024-01-13 04:36:17 +0000

    s

--- main.ts
+++ main.ts
@@ -1,20 +1,15 @@
-/**
- * @license
- * Copyright 2017 Google Inc.
- * SPDX-License-Identifier: Apache-2.0
- */
 
-'use strict';
+import { chromium } from "playwright";
 
-import puppeteer from 'puppeteer';
 
-(async () => {
-  const browser = await puppeteer.launch({
+  const browser = await chromium.launch({
     executablePath: "/usr/bin/chromium",
     args: ['--no-sandbox']
   });
   const page = await browser.newPage();
-  await page.goto('http://example.com');
-  await page.screenshot({path: 'example.png'});
+  await page.goto("https://example.com/");
+  await page.waitForTimeout(2000);
+
+  const screenshot = await page.screenshot();
   await browser.close();
-})();
+  console.log("Screenshot taken");
